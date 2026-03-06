f95378d457324e9c3a7659e95453b04c9d36c639
juanpulgarin09 <juanpulgarin.09az@gmail.com>
2026-03-05 21:39:34 -0500

    Add Rhombus

---
+++ LogicConcepts/Rhombus/Program.cs
@@ -0,0 +1,57 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    // Data input
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.Clear();
+    Console.WriteLine("*** ROMBO ***");
+    var n = ConsoleExtension.GetInt("Ingrese el tamaño del rombo: ");
+
+    // Show results
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Clear();
+
+    ShowRhombus(n);
+
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.ForegroundColor = ConsoleColor.White;
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+void ShowRhombus(int n)
+{
+    // Mitad superior
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write(new string(' ', n - i));
+        Console.Write("#");
+        if (i > 1)
+        {
+            Console.Write(new string(' ', 2 * i - 3));
+            Console.Write("#");
+        }
+        Console.WriteLine();
+    }
+
+    // Mitad inferior
+    for (int i = n - 1; i >= 1; i--)
+    {
+        Console.Write(new string(' ', n - i));
+        Console.Write("#");
+        if (i > 1)
+        {
+            Console.Write(new string(' ', 2 * i - 3));
+            Console.Write("#");
+        }
+        Console.WriteLine();
+    }
+}
